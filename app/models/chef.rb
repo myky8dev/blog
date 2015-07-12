c3d7392bd36f3dd2288a385f3463b9c1bacc002b
myky8dev <myky8dev@gmail.com>
2015-07-12 17:11:48 +0000

    Changes

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,7 @@
 class Chef < ActiveRecord::Base
     has_many :recipes
+    before_save { self.email = email.downcase }
+    validates :chefname, presence: true, length: { minimum: 10, maximum: 40 }
+    VALID_EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+    validates :email, presence: true, length: { minimum: 10, maximum: 100 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
 end
